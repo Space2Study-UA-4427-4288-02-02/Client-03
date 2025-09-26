deebab6b6c13966265345f36d6cab20ce7afc81b
Kseniia-Mekheda <ksenia.mekheda@gmail.com>
2025-09-26 15:58:59 +0300

    Added import of vitest

--- src/tests/unit/components/search-filter-input/SearchFilterInput.spec.jsx
+++ src/tests/unit/components/search-filter-input/SearchFilterInput.spec.jsx
@@ -1,3 +1,4 @@
+import { vi } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import SearchFilterInput from '~/components/search-filter-input/SearchFilterInput'
