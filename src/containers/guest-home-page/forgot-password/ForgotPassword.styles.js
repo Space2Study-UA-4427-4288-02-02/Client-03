3c0d2ef0707fe15c3aa62fbf60fe699c5b9159be
Kseniia-Mekheda <ksenia.mekheda@gmail.com>
2025-10-01 17:24:17 +0300

    Fixed bug with text overlapping the input field

--- src/containers/guest-home-page/forgot-password/ForgotPassword.styles.js
+++ src/containers/guest-home-page/forgot-password/ForgotPassword.styles.js
@@ -17,7 +17,8 @@ export const styles = {
   titleWithDescription: {
     wrapper: {
       textAlign: 'start',
-      m: 0
+      m: 0,
+      pb: '20px'
     },
     title: {
       typography: 'h5',
