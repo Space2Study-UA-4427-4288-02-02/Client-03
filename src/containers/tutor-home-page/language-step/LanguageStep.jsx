47689c778a9d2c550716104aa88cd079d8e8bcb4
Lana Kostyshyn <165199527+annabelle-9@users.noreply.github.com>
2025-10-30 19:11:04 +0200

    Tutor stepper. Language #9 (#21)
    * Tutor stepper. Language #9
    
    * changed from common.languages to LanguagesEnum
    
    * added translations of LanguagesEnum
    
    ---------
    
    Co-authored-by: Lana Kostyshyn <esetrilaine1999@gmail.com>

--- src/containers/tutor-home-page/language-step/LanguageStep.jsx
+++ src/containers/tutor-home-page/language-step/LanguageStep.jsx
@@ -2,15 +2,40 @@ import Box from '@mui/material/Box'
 
 import { styles } from '~/containers/tutor-home-page/language-step/LanguageStep.styles'
 import img from '~/assets/img/tutor-home-page/become-tutor/languages.svg'
+import { useTranslation } from 'react-i18next'
+import { useStepContext } from '~/context/step-context'
+import { languages } from '~/constants'
+import AppAutoComplete from '~/components/app-auto-complete/AppAutoComplete'
+import Typography from '@mui/material/Typography'
 
 const LanguageStep = ({ btnsBox }) => {
+  const { t } = useTranslation()
+  const { stepData, handleStepData } = useStepContext()
+
   return (
     <Box sx={styles.container}>
       <Box sx={styles.imgContainer}>
         <Box component='img' src={img} sx={styles.img} />
       </Box>
       <Box sx={styles.rigthBox}>
-        <Box>Language here</Box>
+        <Typography sx={styles.title}>
+          {t('becomeTutor.languages.title')}
+        </Typography>
+        <Box sx={{ mb: 2 }}>
+          <AppAutoComplete
+            onChange={(_, value) => {
+              if (value) {
+                handleStepData('language', value)
+              }
+            }}
+            options={Object.values(languages)}
+            textFieldProps={{
+              label: t('becomeTutor.languages.autocompleteLabel'),
+              fullWidth: true
+            }}
+            value={stepData['language'] ?? null}
+          />
+        </Box>
         {btnsBox}
       </Box>
     </Box>
